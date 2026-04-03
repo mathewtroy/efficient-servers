a80ebaa1b3d7bb7c0bf04d829d779fd36017a1a6
Aleksandr Kross <krossale@fel.cvut.cz>
2026-04-03 17:19:41 +0200

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
+#include "server.hpp"
 #include <iostream>
 
 int main(int argc, char** argv) {
-    std::cout << "Server starting..." << std::endl;
+    int port = 1234;
+
+    if (argc > 1) {
+        port = std::stoi(argv[1]);
+    }
+
+    Server server(port);
+    server.run();
+
     return 0;
 }
