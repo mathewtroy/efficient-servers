5912a3933efd07fb0c1924ce74781f70a2020924
Aleksandr Kross <krossale@fel.cvut.cz>
2026-04-03 17:19:32 +0200

    update implementation of Dijkstra

--- src/dijkstra.hpp
+++ src/dijkstra.hpp
@@ -1,8 +1,28 @@
 #pragma once
 
 #include <cstdint>
+#include <vector>
+
+#include "types.hpp"
 
 class Dijkstra {
 public:
-    Dijkstra() = default;
+    struct Edge {
+        uint32_t to;
+        uint32_t weight;
+    };
+
+    using Graph = std::vector<std::vector<Edge>>;
+
+    static bool shortest_path(
+        const Graph& graph,
+        uint32_t source,
+        uint32_t target,
+        uint64_t& result
+    );
+
+    static uint64_t sum_of_shortest_paths(
+        const Graph& graph,
+        uint32_t source
+    );
 };
